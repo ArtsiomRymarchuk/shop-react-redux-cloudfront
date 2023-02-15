140288d16da49000f50434f7c2ff2e1cf5f1fe39
ArtsiomRymarchuk <artsiom.rymarchuk@onepeloton.com>
2023-02-15 16:47:59 +0300

    get all products

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://0lsn7wv9bk.execute-api.us-east-1.amazonaws.com/dev/api",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://0lsn7wv9bk.execute-api.us-east-1.amazonaws.com/dev/api",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
