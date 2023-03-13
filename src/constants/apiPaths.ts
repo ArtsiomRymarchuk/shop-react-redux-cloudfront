add673ff6027d462a48cd89f1ed3fdb9657d133d
ArtsiomRymarchuk <artsiom.rymarchuk@onepeloton.com>
2023-03-13 09:59:01 +0300

    upload file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://0lsn7wv9bk.execute-api.us-east-1.amazonaws.com/dev/api",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://ma2dxw70o7.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://0lsn7wv9bk.execute-api.us-east-1.amazonaws.com/dev/api",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
